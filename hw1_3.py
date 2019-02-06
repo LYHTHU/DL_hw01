3d5a6f4afb3c2c1c0a2fb3b278fb46ce038f487e
yunhaoli <yunhaoli.cs@gmail.com>
2019-02-06 15:37:54 -0500

    Working on 3

--- hw1_3.py
+++ hw1_3.py
@@ -14,3 +14,27 @@ if cuda:
     torch.cuda.manual_seed_all(seed)
 
 X = torch.randn(100, 2)
+
+
+class Linear_fc_relu(torch.nn.Module):
+    def __init__(self):
+        super(Linear_fc_relu, self).__init__()
+
+    def forward(self, input):
+        return
+
+
+
+class Linear_fc_sigmoid(torch.nn.Module):
+    def __init__(self):
+        super(Linear_fc_sigmoid, self).__init__()
+
+    def forward(self, input):
+        return
+
+
+linear_fc_relu = Linear_fc_relu()
+linear_fc_relu.forward(X)
+
+linear_fc_sigmoid = Linear_fc_sigmoid()
+linear_fc_sigmoid.forward(X)
